1dc35582778c10d0df09e0341d2e059de8730b91
Yashow_hoo <55846954+Yashow98@users.noreply.github.com>
2022-05-07 23:14:22 +0800

    Update coco_detection.py
    modify test

--- mmdetection/configs/_base_/datasets/coco_detection.py
+++ mmdetection/configs/_base_/datasets/coco_detection.py
@@ -43,7 +43,7 @@ data = dict(
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        ann_file=data_root + 'annotations/instances_train2017.json',
+        img_prefix=data_root + 'train2017/',
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='bbox')
