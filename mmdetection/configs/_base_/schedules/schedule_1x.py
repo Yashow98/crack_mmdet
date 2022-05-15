1154954d6aa1295d9475a8cf972615c3945a0fc5
Yashow_hoo <55846954+Yashow98@users.noreply.github.com>
2022-05-15 14:53:15 +0800

    Update schedule_1x.py

--- mmdetection/configs/_base_/schedules/schedule_1x.py
+++ mmdetection/configs/_base_/schedules/schedule_1x.py
@@ -4,7 +4,7 @@ optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
     policy='step',
-    warmup='constant',
+    warmup='linear',
     warmup_iters=500,
     warmup_ratio=0.001,
     step=[8, 11])
